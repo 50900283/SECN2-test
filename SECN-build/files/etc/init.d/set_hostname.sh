437c79d921b5e6b26c0f94951d5984ff86dee4c8
Terry Gillett <tgillett@gmail.com>
2013-04-12 23:10:06 +1000

    Added files for UBNT and AR23 Devices

--- SECN-build/files/etc/init.d/set_hostname.sh
+++ SECN-build/files/etc/init.d/set_hostname.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
-# Build the hostname (TP-nnn) from the device IP address last octet
+# Set the host name prefix
+HOST="TP"
+
+# Build the hostname ($HOST-nnn) from the device IP address last octet
 OCTET_D=`uci show network.lan.ipaddr | cut -d = -f2 | cut -d . -f4`
 
 # Set the hostname
-uci set system.@system[0].hostname=TP-$OCTET_D
+uci set system.@system[0].hostname=$HOST-$OCTET_D
 uci commit system
 
 # Set the hostname as the Common Name in the SSL certificate for the web server.
-uci set uhttpd.px5g.commonname=TP-$OCTET_D
+uci set uhttpd.px5g.commonname=$HOST-$OCTET_D
 uci commit uhttpd
 
 # Set the system hostname
