582adb987c6f4d514830b9054c15e7b337cc7c6d
Terry Gillett <tgillett@gmail.com>
2013-04-12 12:25:48 +1000

    Update to Build-TP.sh script for directory naming

--- Build-TP.sh
+++ Build-TP.sh
@@ -15,29 +15,130 @@ fi
 
 echo "Start build process"
 
-echo "Set up new directory name with date"
-DATE=`date +%Y-%m-%d-%H:%M`
-DIR=$DATE"-TP"
+# Set up version strings
+VER="Version 2.0 Beta 1e (r34386)"
+DIRVER="B-1e"
 
 ###########################
 
-echo " Copy files from Git repo into build folder"
+echo "Copy files from Git repo into build folder"
 rm -r ./SECN-build/files/*
 cp -r -f ~/Git/SECN2-test/SECN-build/* ./SECN-build/
 
 ###########################
 
+echo "Set up new directory name with date and version"
+DATE=`date +%Y-%m-%d-%H:%M`
+DIR=$DATE"-TP-"$DIRVER
+
 # Set up build directory
-echo "Set up new build directory  ./bin/ar71xx/builds/build-"$DIR
+echo "New build directory  ./bin/ar71xx/builds/build-"$DIR
 mkdir ./bin/ar71xx/builds/build-$DIR
 
 # Create md5sums file
 touch ./bin/ar71xx/builds/build-$DIR/md5sums
 
-# Set up version strings
-VER="Version 2.0 Beta 1 Test (r34386)"
-
 ###########################
+
+echo '----------------------------'
+
+
+echo "Set up files for WR842 "
+DEVICE="WR842"
+
+rm -r ./files/*
+cp -r ./SECN-build/files       .        ; echo "Copy generic files"
+cp -r ./SECN-build/WR842/files .        ; echo "Overlay device specific files"
+./FactoryRestore.sh											; echo "Build Factory Restore tar file"
+echo "Check files "
+ls -al ./files   
+echo ""
+
+# Set up version file
+echo "Version: "  $VER $DEVICE
+echo $VER  " TP-Link " $DEVICE   > ./files/etc/secn_version
+echo "Date stamp the version file: " $DATE
+echo "Build date " $DATE         >> ./files/etc/secn_version
+echo " "                         >> ./files/etc/secn_version
+
+echo "Set up .config for WR842"
+rm ./.config
+cp ./SECN-build/WR842/.config  ./.config
+make defconfig > /dev/null
+## Use for first build on a new revision to update .config file
+cp ./.config ./SECN-build/WR842/.config 
+
+echo "Check .config version"
+cat ./.config | grep "OpenWrt version"
+echo "Check banner version"
+cat ./files/etc/secn_version | grep "Version"
+echo ""
+
+echo "Run make for WR842"
+make
+
+echo  "Move files to build folder"
+mv ./bin/ar71xx/*-squash*sysupgrade.bin ./bin/ar71xx/builds/build-$DIR
+mv ./bin/ar71xx/*-squash*factory.bin    ./bin/ar71xx/builds/build-$DIR
+echo "Clean up unused files"
+rm ./bin/ar71xx/openwrt-*
+echo "Update md5sums"
+cat ./bin/ar71xx/md5sums | grep "squashfs" | grep ".bin" >> ./bin/ar71xx/builds/build-$DIR/md5sums
+echo ""
+echo "End WR842 build"
+echo ""
+
+##################
+#exit  #  Uncomment to end the build process here
+
+echo '----------------------------'
+
+echo "Set up files for MR3020 "
+DEVICE="MR3020"
+rm -r ./files/*
+cp -r ./SECN-build/files       .        ; echo "Copy generic files"
+cp -r ./SECN-build/MR3020/files .       ; echo "Overlay device specific files"
+./FactoryRestore.sh											; echo "Build Factory Restore tar file"
+echo "Check files "
+ls -al ./files   
+echo ""
+
+# Set up version file
+echo "Version: "  $VER $DEVICE
+echo $VER  " TP-Link " $DEVICE   > ./files/etc/secn_version
+echo "Date stamp the version file: " $DATE
+echo "Build date " $DATE         >> ./files/etc/secn_version
+echo " "                         >> ./files/etc/secn_version
+
+echo "Set up .config for MR3020 "
+rm ./.config
+cp ./SECN-build/MR3020/.config  ./.config
+make defconfig > /dev/null
+## Use for first build on a new revision to update .config file
+cp ./.config ./SECN-build/MR3020/.config 
+
+echo "Check .config version"
+cat ./.config | grep "OpenWrt version"
+echo "Check banner version"
+cat ./files/etc/secn_version | grep "Version"
+echo ""
+
+echo "Run make for MR3020"
+make
+
+echo  "Move files to build folder"
+mv ./bin/ar71xx/*squash*sysupgrade.bin ./bin/ar71xx/builds/build-$DIR
+mv ./bin/ar71xx/*squash*factory.bin    ./bin/ar71xx/builds/build-$DIR
+echo "Clean up unused files"
+rm ./bin/ar71xx/openwrt-*
+echo "Update md5sums"
+cat ./bin/ar71xx/md5sums | grep "squashfs" | grep ".bin" >> ./bin/ar71xx/builds/build-$DIR/md5sums
+echo ""
+echo "End MR3020 build"
+echo ""
+
+##################
+#exit  #  Uncomment to end the build process here
 
 echo '----------------------------'
 
@@ -90,56 +191,6 @@ echo "End WR703 build"
 echo ""
 
 ###############
-#exit  #  Uncomment to end the build process here
-
-echo '----------------------------'
-
-echo "Set up files for WR842 "
-DEVICE="WR842"
-
-rm -r ./files/*
-cp -r ./SECN-build/files       .        ; echo "Copy generic files"
-cp -r ./SECN-build/WR842/files .        ; echo "Overlay device specific files"
-./FactoryRestore.sh											; echo "Build Factory Restore tar file"
-echo "Check files "
-ls -al ./files   
-echo ""
-
-# Set up version file
-echo "Version: "  $VER $DEVICE
-echo $VER  " TP-Link " $DEVICE   > ./files/etc/secn_version
-echo "Date stamp the version file: " $DATE
-echo "Build date " $DATE         >> ./files/etc/secn_version
-echo " "                         >> ./files/etc/secn_version
-
-echo "Set up .config for WR842"
-rm ./.config
-cp ./SECN-build/WR842/.config  ./.config
-make defconfig > /dev/null
-## Use for first build on a new revision to update .config file
-cp ./.config ./SECN-build/WR842/.config 
-
-echo "Check .config version"
-cat ./.config | grep "OpenWrt version"
-echo "Check banner version"
-cat ./files/etc/secn_version | grep "Version"
-echo ""
-
-echo "Run make for WR842"
-make
-
-echo  "Move files to build folder"
-mv ./bin/ar71xx/*-squash*sysupgrade.bin ./bin/ar71xx/builds/build-$DIR
-mv ./bin/ar71xx/*-squash*factory.bin    ./bin/ar71xx/builds/build-$DIR
-echo "Clean up unused files"
-rm ./bin/ar71xx/openwrt-*
-echo "Update md5sums"
-cat ./bin/ar71xx/md5sums | grep "squashfs" | grep ".bin" >> ./bin/ar71xx/builds/build-$DIR/md5sums
-echo ""
-echo "End WR842 build"
-echo ""
-
-##################
 #exit  #  Uncomment to end the build process here
 
 
@@ -196,55 +247,6 @@ echo ""
 
 echo '----------------------------'
 
-
-echo "Set up files for MR3020 "
-DEVICE="MR3020"
-rm -r ./files/*
-cp -r ./SECN-build/files       .        ; echo "Copy generic files"
-cp -r ./SECN-build/MR3020/files .       ; echo "Overlay device specific files"
-./FactoryRestore.sh											; echo "Build Factory Restore tar file"
-echo "Check files "
-ls -al ./files   
-echo ""
-
-# Set up version file
-echo "Version: "  $VER $DEVICE
-echo $VER  " TP-Link " $DEVICE   > ./files/etc/secn_version
-echo "Date stamp the version file: " $DATE
-echo "Build date " $DATE         >> ./files/etc/secn_version
-echo " "                         >> ./files/etc/secn_version
-
-echo "Set up .config for MR3020 "
-rm ./.config
-cp ./SECN-build/MR3020/.config  ./.config
-make defconfig > /dev/null
-## Use for first build on a new revision to update .config file
-cp ./.config ./SECN-build/MR3020/.config 
-
-echo "Check .config version"
-cat ./.config | grep "OpenWrt version"
-echo "Check banner version"
-cat ./files/etc/secn_version | grep "Version"
-echo ""
-
-echo "Run make for MR3020"
-make
-
-echo  "Move files to build folder"
-mv ./bin/ar71xx/*squash*sysupgrade.bin ./bin/ar71xx/builds/build-$DIR
-mv ./bin/ar71xx/*squash*factory.bin    ./bin/ar71xx/builds/build-$DIR
-echo "Clean up unused files"
-rm ./bin/ar71xx/openwrt-*
-echo "Update md5sums"
-cat ./bin/ar71xx/md5sums | grep "squashfs" | grep ".bin" >> ./bin/ar71xx/builds/build-$DIR/md5sums
-echo ""
-echo "End MR3020 build"
-echo ""
-
-##################
-#exit  #  Uncomment to end the build process here
-
-echo '----------------------------'
 
 echo "Set up files for MR11U "
 DEVICE="MR11U"
